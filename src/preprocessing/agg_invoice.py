44a1807fc8956e4c5ba5c4027481861919650098
Felix Geyer <geyer.felg@gmail.com>
2023-08-30 16:51:16 +0200

    aggregation fun corrected

--- src/preprocessing/agg_invoice.py
+++ src/preprocessing/agg_invoice.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def agg_invoice_num_mode_no_monthly_weighting(
         df_invoice,
-        cols_to_agg_mean=["consommation_level_1","consommation_level_2",
+        cols_to_agg_num=["consommation_level_1","consommation_level_2",
                           "consommation_level_3","consommation_level_4",
                           "counter_coefficient", "months_number",
                           "invoice_date"
@@ -11,10 +11,9 @@ def agg_invoice_num_mode_no_monthly_weighting(
                             "counter_code", "reading_remarque",
                             "counter_type"],
         num_agg = "mean"):
-    df_num =df_invoice.head(100).groupby("client_id")[cols_to_agg_num].agg(num_agg)
-    df_mode = df_invoice.head(100).groupby("client_id")[cols_to_agg_mode].apply(lambda x: x.mode().iloc[0])
+    df_num = df_invoice.groupby("client_id")[cols_to_agg_num].agg(num_agg)
+    df_mode = df_invoice.groupby("client_id")[cols_to_agg_mode].apply(lambda x: x.mode().iloc[0])
     df_ges = pd.concat([df_num,df_mode], axis=1)
-    df_ges = df_num
     df_ges.reset_index(inplace=True)
     return df_ges
 
