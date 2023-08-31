55fb47e6f3240a2905ebda4784bd13e2622aacb4
Felix Geyer <geyer.felg@gmail.com>
2023-08-31 21:36:19 +0200

    Docstring adding and commenting

--- src/models/basemodel.py
+++ src/models/basemodel.py
@@ -1,8 +1,20 @@
+""" This module contains the basemodel used as a benchmark for the project """
 import pandas as pd
 import numpy as np
 
 def basemodel_predict_proba(df:pd.DataFrame)->pd.Series:
-    y_pred = (df.client_catg == 51)  | (df.counter_code.isin([40,25])) #| ((df.consommation_level_1 <10000)& (df.consommation_level_2 <10000)& (df.consommation_level_3 <10000)& (df.consommation_level_4 <10000))
+    """ Simple heuristic to predict the probability of a customer to be fraudulent based on the EDA
+        findings that fraudulent customers are more likely to be in client_category 51 or have a
+        counter_code of 40 or 25.
+    Args:
+        df (pd.DataFrame): DataFrame containing the invoice data.
+    Returns:
+        pd.Series: Series containing the probability of churn for each client_id in df.
+    """
+    y_pred = (df.client_catg == 51)  | (df.counter_code.isin([40,25]))
+    # Observation that fraudulent customers have a lower consumption level did not improve the model
+    #| ((df.consommation_level_1 <10000)& (df.consommation_level_2 <10000)&
+    #  (df.consommation_level_3 <10000)& (df.consommation_level_4 <10000))
     y_proba = pd.Series(np.zeros(len(df)))
     y_proba.index = df.index
     y_proba[y_pred] = 1
--- src/preprocessing/agg_invoice.py
+++ src/preprocessing/agg_invoice.py
@@ -1,5 +1,6 @@
+""" Module to aggregate invoice data by client_id."""
+
 import pandas as pd
-import numpy as np
 
 def agg_invoice_num_mode_no_monthly_weighting(
         df_invoice,
@@ -12,6 +13,16 @@ def agg_invoice_num_mode_no_monthly_weighting(
                             "counter_code", "reading_remarque",
                             "counter_type"],
         num_agg = "mean"):
+    """ Aggregate invoice data by client_id, using the mean for numerical features in cols_to_agg_num, and the mode for categorical features in cols_to_agg_mode.
+    Args:
+        df_invoice (pd.DataFrame): invoice data
+        cols_to_agg_num (list, optional): Numerical features to aggregate using the mean. Defaults to ["consommation_level_1","consommation_level_2", "consommation_level_3","consommation_level_4", "counter_coefficient", "months_number", "invoice_date"].
+        cols_to_agg_mode (list, optional): Categorical features to aggregate using the mode. Defaults to ["tarif_type", "counter_statue", "counter_code", "reading_remarque", "counter_type"].
+        num_agg (str, optional): Aggregation method for numerical features in cols_to_agg_num. Defaults to "mean".
+
+    Returns:
+        pd.DataFrame: updated DataFrame
+    """
     df_num = df_invoice.groupby("client_id")[cols_to_agg_num].agg(num_agg)
     df_mode = df_invoice.groupby("client_id")[cols_to_agg_mode].apply(lambda x: x.mode().iloc[0])
     df_ges = pd.concat([df_num,df_mode], axis=1)
@@ -20,7 +31,7 @@ def agg_invoice_num_mode_no_monthly_weighting(
 
 
 def agg_invoice_num_mode_monthly_weighting(
-        df_invoice,
+        df_invoice:pd.DataFrame,
         cols_to_agg_num=["months_number",
                          "invoice_date",
                          "counter_coefficient"],
@@ -30,12 +41,25 @@ def agg_invoice_num_mode_monthly_weighting(
         cols_to_agg_mode = ["tarif_type", "counter_statue",
                             "counter_code", "reading_remarque",
                             "counter_type"],
-        num_agg = "mean"):
+        num_agg = "mean")->pd.DataFrame:
+    """ Aggregate invoice data by client_id, using the mean for numerical features in cols_to_agg_num, the weighted mean for numerical features in cols_to_agg_num_weighted, 
+        and the mode for categorical features in cols_to_agg_mode.
+    Args:
+        df_invoice (pd.DataFrame): invoice data
+        cols_to_agg_num (list, optional): Numerical features to aggregate using the mean. Defaults to ["months_number", "invoice_date", "counter_coefficient"].
+        cols_to_agg_num_weighted (list, optional): Numerical features to aggregate using the weighted mean. Defaults to ["consommation_level_1","consommation_level_2", "consommation_level_3","consommation_level_4", ].
+        cols_to_agg_mode (list, optional): Categorical features to aggregate using the mode. Defaults to ["tarif_type", "counter_statue", "counter_code", "reading_remarque", "counter_type"].
+        num_agg (str, optional): Aggregation method for numerical features in cols_to_agg_num. Defaults to "mean".
 
+    Returns:
+        pd.DataFrame: updated DataFrame
+    """
+    # Aggregation of weighted numerical features by months_number
     df_num_weighted= df_invoice.groupby("client_id")[cols_to_agg_num_weighted+["months_number"]].sum()
     for col in cols_to_agg_num_weighted: 
         df_num_weighted[col] = df_num_weighted[col]/ df_num_weighted["months_number"]
     df_num_weighted.drop(columns="months_number",inplace=True)
+    # Aggregation of numerical and categorical features
     df_num = df_invoice.groupby("client_id")[cols_to_agg_num].agg(num_agg)
     df_mode = df_invoice.groupby("client_id")[cols_to_agg_mode].apply(lambda x: x.mode().iloc[0])
     df_ges = pd.concat([df_num,df_mode,df_num_weighted], axis=1)
