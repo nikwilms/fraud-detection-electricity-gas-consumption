0587ea288c01b50b9c6b7ab550709d0e4d2c1fe4
nikitawilms <nikdotde@gmail.com>
2023-09-07 10:30:43 +0200

    applied formatting

--- src/models/basemodel.py
+++ src/models/basemodel.py
@@ -2,8 +2,9 @@
 import pandas as pd
 import numpy as np
 
-def basemodel_predict_proba(df:pd.DataFrame)->pd.Series:
-    """ Simple heuristic to predict the probability of a customer to be fraudulent based on the EDA
+
+def basemodel_predict_proba(df: pd.DataFrame) -> pd.Series:
+    """Simple heuristic to predict the probability of a customer to be fraudulent based on the EDA
         findings that fraudulent customers are more likely to be in client_category 51 or have a
         counter_code of 40 or 25.
     Args:
@@ -11,12 +12,11 @@ def basemodel_predict_proba(df:pd.DataFrame)->pd.Series:
     Returns:
         pd.Series: Series containing the probability of churn for each client_id in df.
     """
-    y_pred = (df.client_catg == 51)  | (df.counter_code.isin([40,25]))
+    y_pred = (df.client_catg == 51) | (df.counter_code.isin([40, 25]))
     # Observation that fraudulent customers have a lower consumption level did not improve the model
-    #| ((df.consommation_level_1 <10000)& (df.consommation_level_2 <10000)&
+    # | ((df.consommation_level_1 <10000)& (df.consommation_level_2 <10000)&
     #  (df.consommation_level_3 <10000)& (df.consommation_level_4 <10000))
     y_proba = pd.Series(np.zeros(len(df)))
     y_proba.index = df.index
     y_proba[y_pred] = 1
     return y_proba
-
--- src/preprocessing/agg_invoice.py
+++ src/preprocessing/agg_invoice.py
@@ -5,17 +5,26 @@ import numpy as np
 
 
 def agg_invoice_num_mode_no_monthly_weighting(
-        df_invoice,
-        cols_to_agg_num=["consommation_level_1", "consommation_level_2",
-                         "consommation_level_3", "consommation_level_4",
-                         "counter_coefficient", "months_number",
-                         "invoice_date"
-                         ],
-        cols_to_agg_mode=["tarif_type", "counter_statue",
-                          "counter_code", "reading_remarque",
-                          "counter_type"],
-        num_agg="mean"):
-    """ Aggregate invoice data by client_id, using the mean for numerical features in
+    df_invoice,
+    cols_to_agg_num=[
+        "consommation_level_1",
+        "consommation_level_2",
+        "consommation_level_3",
+        "consommation_level_4",
+        "counter_coefficient",
+        "months_number",
+        "invoice_date",
+    ],
+    cols_to_agg_mode=[
+        "tarif_type",
+        "counter_statue",
+        "counter_code",
+        "reading_remarque",
+        "counter_type",
+    ],
+    num_agg="mean",
+):
+    """Aggregate invoice data by client_id, using the mean for numerical features in
         cols_to_agg_num, and the mode for categorical features in cols_to_agg_mode.
     Args:
         df_invoice (pd.DataFrame): invoice data
@@ -33,26 +42,33 @@ def agg_invoice_num_mode_no_monthly_weighting(
     """
     df_num = df_invoice.groupby("client_id")[cols_to_agg_num].agg(num_agg)
     df_mode = df_invoice.groupby("client_id")[cols_to_agg_mode].apply(
-        lambda x: x.mode().iloc[0])
+        lambda x: x.mode().iloc[0]
+    )
     df_ges = pd.concat([df_num, df_mode], axis=1)
     df_ges.reset_index(inplace=True)
     return df_ges
 
 
 def agg_invoice_num_mode_monthly_weighting(
-        df_invoice: pd.DataFrame,
-        cols_to_agg_num=["months_number",
-                         "invoice_date",
-                         "counter_coefficient"],
-        cols_to_agg_num_weighted=["consommation_level_1", "consommation_level_2",
-                                  "consommation_level_3", "consommation_level_4",
-                                  ],
-        cols_to_agg_mode=["tarif_type", "counter_statue",
-                          "counter_code", "reading_remarque",
-                          "counter_type"],
-        num_agg="mean") -> pd.DataFrame:
-    """ Aggregate invoice data by client_id, using the mean for numerical features in
-        cols_to_agg_num, the weighted mean for numerical features in cols_to_agg_num_weighted, 
+    df_invoice: pd.DataFrame,
+    cols_to_agg_num=["months_number", "invoice_date", "counter_coefficient"],
+    cols_to_agg_num_weighted=[
+        "consommation_level_1",
+        "consommation_level_2",
+        "consommation_level_3",
+        "consommation_level_4",
+    ],
+    cols_to_agg_mode=[
+        "tarif_type",
+        "counter_statue",
+        "counter_code",
+        "reading_remarque",
+        "counter_type",
+    ],
+    num_agg="mean",
+) -> pd.DataFrame:
+    """Aggregate invoice data by client_id, using the mean for numerical features in
+        cols_to_agg_num, the weighted mean for numerical features in cols_to_agg_num_weighted,
         and the mode for categorical features in cols_to_agg_mode.
     Args:
         df_invoice (pd.DataFrame): invoice data
@@ -71,70 +87,100 @@ def agg_invoice_num_mode_monthly_weighting(
         pd.DataFrame: updated DataFrame
     """
     # Aggregation of weighted numerical features by months_number
-    df_num_weighted = df_invoice.groupby(
-        "client_id")[cols_to_agg_num_weighted+["months_number"]].sum()
+    df_num_weighted = df_invoice.groupby("client_id")[
+        cols_to_agg_num_weighted + ["months_number"]
+    ].sum()
     for col in cols_to_agg_num_weighted:
-        df_num_weighted[col] = df_num_weighted[col] / \
-            df_num_weighted["months_number"]
+        df_num_weighted[col] = df_num_weighted[col] / df_num_weighted["months_number"]
     df_num_weighted.drop(columns="months_number", inplace=True)
     # Aggregation of numerical and categorical features
     df_num = df_invoice.groupby("client_id")[cols_to_agg_num].agg(num_agg)
     df_mode = df_invoice.groupby("client_id")[cols_to_agg_mode].apply(
-        lambda x: x.mode().iloc[0])
+        lambda x: x.mode().iloc[0]
+    )
     df_ges = pd.concat([df_num, df_mode, df_num_weighted], axis=1)
     df_ges.reset_index(inplace=True)
     return df_ges
 
 
-
 def agg_cons_monthly_weighting(df, cols_to_agg_num_weighted, suffix=""):
-    df_monthly_weighted = df.groupby(
-    ["client_id"])[cols_to_agg_num_weighted+["months_number"]].sum()
+    df_monthly_weighted = df.groupby(["client_id"])[
+        cols_to_agg_num_weighted + ["months_number"]
+    ].sum()
     for col in cols_to_agg_num_weighted:
-        df_monthly_weighted[col+suffix] = df_monthly_weighted[col] / \
-            df_monthly_weighted["months_number"]
+        df_monthly_weighted[col + suffix] = (
+            df_monthly_weighted[col] / df_monthly_weighted["months_number"]
+        )
         df_monthly_weighted.drop(columns=col, inplace=True)
     df_monthly_weighted.drop(columns="months_number", inplace=True)
     return df_monthly_weighted
 
 
 def agg_invoice_smart(
-        df_invoice,
-cols_to_agg_min = ["invoice_date"],
-cols_to_agg_num=["counter_coefficient"],
-cols_to_agg_num_weighted=["consommation_level_1", "consommation_level_2",
-                            "consommation_level_3", "consommation_level_4",
-                            ],
-cols_to_agg_mode=["tarif_type", "counter_statue",
-                    "counter_code", "reading_remarque"],
-num_agg="mean"):
-    df_counter_type_elec = df_invoice[df_invoice.counter_type=="ELEC"]
-    df_counter_type_gaz = df_invoice[df_invoice.counter_type=="GAZ"]
+    df_invoice,
+    cols_to_agg_min=["invoice_date"],
+    cols_to_agg_num=["counter_coefficient"],
+    cols_to_agg_num_weighted=[
+        "consommation_level_1",
+        "consommation_level_2",
+        "consommation_level_3",
+        "consommation_level_4",
+    ],
+    cols_to_agg_mode=[
+        "tarif_type",
+        "counter_statue",
+        "counter_code",
+        "reading_remarque",
+    ],
+    num_agg="mean",
+):
+    df_counter_type_elec = df_invoice[df_invoice.counter_type == "ELEC"]
+    df_counter_type_gaz = df_invoice[df_invoice.counter_type == "GAZ"]
 
-    df_monthly_weighted_elec = agg_cons_monthly_weighting(df_counter_type_elec, cols_to_agg_num_weighted, "_elec")
+    df_monthly_weighted_elec = agg_cons_monthly_weighting(
+        df_counter_type_elec, cols_to_agg_num_weighted, "_elec"
+    )
 
-    df_monthly_weighted_gaz = agg_cons_monthly_weighting(df_counter_type_gaz, cols_to_agg_num_weighted, "_gaz")
+    df_monthly_weighted_gaz = agg_cons_monthly_weighting(
+        df_counter_type_gaz, cols_to_agg_num_weighted, "_gaz"
+    )
 
     # aggregation of features by min
-    df_min= df_invoice.groupby(["client_id"])[cols_to_agg_min].min()
+    df_min = df_invoice.groupby(["client_id"])[cols_to_agg_min].min()
 
     # add duration of customer relationship
-    df_invoice['invoice_date'] = pd.to_datetime(df_invoice['invoice_date'])
+    df_invoice["invoice_date"] = pd.to_datetime(df_invoice["invoice_date"])
     df_max_date = df_invoice.groupby(["client_id"])["invoice_date"].max()
     df_min_date = df_invoice.groupby(["client_id"])["invoice_date"].min()
-    df_date = (df_max_date - df_min_date)
+    df_date = df_max_date - df_min_date
     # convert to months
     df_date_months = (df_date.dt.days / 30.44).apply(np.ceil).astype(int)
-    df_date_months = pd.DataFrame(df_date_months).rename(columns={"invoice_date":"months_number"})
+    df_date_months = pd.DataFrame(df_date_months).rename(
+        columns={"invoice_date": "months_number"}
+    )
 
     # Aggregation of numerical and categorical features
-    df_date = df_invoice.groupby(["client_id"])[["invoice_date"]].max()- df_invoice.groupby(["invoice_date"])[cols_to_agg_min].min()
+    df_date = (
+        df_invoice.groupby(["client_id"])[["invoice_date"]].max()
+        - df_invoice.groupby(["invoice_date"])[cols_to_agg_min].min()
+    )
 
     df_num = df_invoice.groupby(["client_id"])[cols_to_agg_num].agg(num_agg)
     df_mode = df_invoice.groupby(["client_id"])[cols_to_agg_mode].apply(
-        lambda x: x.mode().iloc[0])
-    #df_ges = pd.concat([df_num, df_mode, df_num_weighted], axis=1)
-    df_ges = pd.concat([df_min, df_date_months, df_num,  df_mode, df_monthly_weighted_elec,df_monthly_weighted_gaz], axis=1)
+        lambda x: x.mode().iloc[0]
+    )
+    # df_ges = pd.concat([df_num, df_mode, df_num_weighted], axis=1)
+    df_ges = pd.concat(
+        [
+            df_min,
+            df_date_months,
+            df_num,
+            df_mode,
+            df_monthly_weighted_elec,
+            df_monthly_weighted_gaz,
+        ],
+        axis=1,
+    )
     df_ges.fillna(0, inplace=True)
     df_ges.reset_index(inplace=True)
     return df_ges
